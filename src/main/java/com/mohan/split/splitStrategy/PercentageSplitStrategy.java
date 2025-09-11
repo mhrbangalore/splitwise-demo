b4cf911c70087a6b85e1a96f486fba88696f4e9b
mhrbangalore <mkhrnexp@gmail.com>
2025-09-11 11:05:14 +0100

    implement percentage strategy

--- src/main/java/com/mohan/split/splitStrategy/PercentageSplitStrategy.java
+++ src/main/java/com/mohan/split/splitStrategy/PercentageSplitStrategy.java
@@ -4,14 +4,13 @@ import com.mohan.split.Split;
 
 import java.util.List;
 
-public class UnEqualSplitStrategy implements ISplitStrategy{
+public class PercentageSplitStrategy implements ISplitStrategy{
     @Override
     public void validateSplitRequest(List<Split> splits, double totalExpenseAmount) {
         double sum = 0;
         for (Split split : splits){
             sum += split.getAmountOwe();
         }
-
         if (sum != totalExpenseAmount){
             throw new IllegalArgumentException("Sum of participants share does not equal the total expense amount");
         }
